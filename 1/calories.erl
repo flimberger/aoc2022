85746766000c1a2258f04b545eb37db99f9a25e3
Florian Limberger <florian.limberger@inovex.de>
2022-12-04 17:06:54 +0100

    [Day 1] Solve puzzle nr. 2
    Use the more generalized list processing to solve pullze #2.

--- 1/calories.erl
+++ 1/calories.erl
@@ -1,13 +1,19 @@
 -module(calories).
--export([run/0]).
+-export([puzzle1/0, puzzle2/0]).
 
 % An elf is a tuple of an index and the carried calories
 
-run() ->
+puzzle1() ->
 	{ok, Fd} = file:open("input", read),
 	AllElves = read_lines(Fd, {1, 0}, []),
-	[{Elf, Calories}|_] = lists:reverse(lists:sort(fun({_, Cl}, {_, Cr}) -> Cl =< Cr end, AllElves)),
+	[{Elf, Calories}|_] = lists:reverse(lists:sort(fun elfsort/2, AllElves)),
 	io:format("Elf #~w: ~w calories~n", [Elf, Calories]).
+
+puzzle2() ->
+	{ok, Fd} = file:open("input", read),
+	[{_, C1}, {_, C2}, {_, C3}|_] = lists:reverse(lists:sort(fun elfsort/2, read_lines(Fd, {1, 0}, []))),
+	Sum = C1 + C2 + C3,
+	io:format("Sum of the top 3 calories: ~w~n", [Sum]).
 
 read_lines(Fd, Acc={Elf, Calories}, List) ->
 	case io:get_line(Fd, '') of
@@ -22,3 +28,5 @@ read_lines(Fd, Acc={Elf, Calories}, List) ->
 trim_newline([]) -> [];
 trim_newline([10]) -> [];
 trim_newline([H|T]) -> [H|trim_newline(T)].
+
+elfsort({_, Cl}, {_, Cr}) -> Cl =< Cr.
