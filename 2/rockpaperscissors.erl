d5a13a72e8c968a1917ca4626d3f36c7f022ac77
Florian Limberger <florian.limberger@inovex.de>
2022-12-07 22:12:39 +0100

    [Day 2] Solve Part 2

--- 2/rockpaperscissors.erl
+++ 2/rockpaperscissors.erl
@@ -1,24 +1,26 @@
 -module(rockpaperscissors).
--export([puzzle1/0]).
+-export([puzzle1/0, puzzle2/0]).
 
 puzzle1() ->
-	L = read_input(),
+	L = read_input(fun map1/2),
 	R = lists:sum(lists:map(fun play/1, L)),
 	io:format("~w~n", [R]).
 
-read_input() ->
+read_input(Mapfun) ->
 	{ok, Fd} = file:open("input", read),
-	read_lines(Fd, []).
+	read_lines(Fd, Mapfun, []).
 
-read_lines(Fd, Lines) ->
+read_lines(Fd, Mapfun, Lines) ->
 	case io:get_line(Fd, '') of
 		eof ->	Lines;
 		Err = {error, _} ->
 			Err;
 		Data ->
 			[A, 32, B, 10] = Data,
-			read_lines(Fd, [{shape(A), shape(B)}|Lines])
+			read_lines(Fd, Mapfun, [Mapfun(A, B)|Lines])
 	end.
+
+map1(A, B) ->	{shape(A), shape(B)}.
 
 shape($A) ->	rock;
 shape($X) ->	rock;
@@ -43,4 +45,30 @@ result(scissors, scissors) ->	{3, 3}.
 
 value(rock) ->	1;
 value(paper) ->	2;
-value(scissors) ->	3.
+value(scissors) ->	3;
+value(lose) ->	0;
+value(draw) ->	3;
+value(win) ->	6.
+
+puzzle2() ->
+	L = read_input(fun map2/2),
+	lists:sum(lists:map(fun({Opponent, Outcome}) -> play2(Opponent, Outcome) end, L)).
+
+map2(A, B) ->	{shape(A), outcome(B)}.
+
+outcome($X) ->	lose;
+outcome($Y) ->	draw;
+outcome($Z) ->	win.
+
+play2(Opponent, Outcome) ->
+	value(turn(Opponent, Outcome)) + value(Outcome).
+
+turn(rock, lose) ->	scissors;
+turn(rock, draw) ->	rock;
+turn(rock, win) ->	paper;
+turn(paper, lose) ->	rock;
+turn(paper, draw) ->	paper;
+turn(paper, win) ->	scissors;
+turn(scissors, lose) ->	paper;
+turn(scissors, draw) ->	scissors;
+turn(scissors, win) ->	rock.
